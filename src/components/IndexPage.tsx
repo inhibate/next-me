9682f4f0ee2f7306ebff75d7cee29eb71cc28a5e
Ivan Pakov <prg938@mail.ru>
2023-09-22 04:23:05 +0300

    Update IndexPage.tsx

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -127,7 +127,7 @@ const IndexPage: FunctionComponent = props => {
         <Me />
         <div className={styles.flexWrapper}>
           <Image src={meJPG} quality={100} alt="PRG938" placeholder="blur" />
-          <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=prg938&layout=compact&hide_border=true&theme=dark&langs_count=8" />
+          <object type="image/svg+xml" data="https://github-readme-stats.vercel.app/api/top-langs/?username=prg938&layout=compact&hide_border=true&theme=dark&langs_count=6" />
         </div>
         <Location />
         <Background />
