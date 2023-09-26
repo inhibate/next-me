d9dcb1a84da3108644df1d974e85149f63761005
Ivan <hypotenuse2@gmail.com>
2023-09-26 14:58:06 +0300

    up

--- src/components/descComponents/blog/index.ts
+++ src/components/descComponents/blog/index.ts
@@ -2,9 +2,11 @@
 import AboutBlogDesc from './AboutBlogDesc'
 import DisqusReactWithColorScheme from './DisqusReactWithColorScheme'
 import FacebookBlockedAccount from './FacebookBlockedAccount'
+import ZustandLibExample from './ZustandLibExample'
 
 export {
   AboutBlogDesc,
   DisqusReactWithColorScheme,
-  FacebookBlockedAccount
+  FacebookBlockedAccount,
+  ZustandLibExample
 }
