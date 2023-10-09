86770d70ef34bea27410de60c310626475d70383
Ivan <hypotenuse2@gmail.com>
2023-10-09 21:57:12 +0300

    asdf

--- src/components/projects/P4.tsx
+++ src/components/projects/P4.tsx
@@ -1,12 +1,20 @@
 import styles from './Item.module.scss'
 import React from 'react'
 import {Paragraph} from '../Typography'
+import Image from 'next/image'
+import screenshot1 from '@/../pub;ic/S31009-203907.jpg'
+import screenshot2 from '@/../pub;ic/S31009-203912.jpg'
 
 export default () => {
   return <div className={styles.desc}>
     <Paragraph>
       <a href='https://prg938.vercel.app/' target='_blank'>prg938.vercel.app</a> using Next Framework.
     </Paragraph>
+    <h2>Screenshots:</h2>
+    <div>
+      <Image src={screenshot1} quality={100} alt="screenshot1" placeholder="blur" style={{borderRadius: 10}} />
+      <Image src={screenshot2} quality={100} alt="screenshot2" placeholder="blur" style={{borderRadius: 10}} />
+    </div>
     <h2>Features:</h2>
     <ul style={{listStyle: 'none'}}>
       <li><b>next/image</b> for image optimization</li>
