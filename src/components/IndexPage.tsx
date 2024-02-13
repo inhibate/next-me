21c5353c38ed377ef17c1e9a04ffc1df2d6198cc
ivan <prg938@mail.ru>
2024-02-14 00:59:39 +0300

    я

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -24,7 +24,7 @@ const IndexPage: FunctionComponent = props => {
       </Head>
       <main className={styles.main}>
         <div className={styles.title}>
-          <div>Привет. Какими судьбами?</div>
+          <div>Привет.</div>
         <div className={styles.myface}>
           <Image src={myFacePhoto} quality={100} alt="PRG938" placeholder="blur" />
         </div>
