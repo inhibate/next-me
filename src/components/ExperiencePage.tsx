f28563c03a800b984b9b9d3666e5789dae13bea9
ivan <prg938@mail.ru>
2024-02-13 23:19:20 +0300

    up

--- src/components/ExperiencePage.tsx
+++ src/components/ExperiencePage.tsx
@@ -10,7 +10,7 @@ export default () => {
     </Head>
     <div className={styles.experience}>
       <div>Экспириенс можно посмотреть тут:</div>
-      <a href="https://career.habr.com/prg938/print">https://career.habr.com/prg938/print</a>
+      <a href="https://career.habr.com/prg938/print">career.habr.com/prg938/print</a>
     </div>
   </>
 }
